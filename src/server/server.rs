bbf6721542ca51a6d371ce89495eb98aed763447
quaisx <alex@braneframe.com>
2023-09-19 12:31:29 -0400

    [thx] spawn a new thread for each new connection

--- src/server/server.rs
+++ src/server/server.rs
@@ -1,7 +1,7 @@
 use std::io::Read;
 use std::net::{Shutdown, TcpListener};
 use crate::http::request::Request;
-
+use std::thread;
 
 /// HTTP Server
 ///     - address: IP address:port to bind to
@@ -25,7 +25,6 @@ impl Server {
         let listener = TcpListener::bind(&self.address).expect("Failed to bind to the address");
 
         // test with netstat -k -l 8080
-
         loop {
             // std::thread::sleep(std::time::Duration::from_millis(500));
             let res = listener.accept();
@@ -38,21 +37,23 @@ impl Server {
                     continue
                 }
             };
-
-            dbg!(format!("[+] new connection {} -> {}", &addr.to_string(), &self.address));
-            let mut data_buf: Vec<u8> = Vec::new();
-            let read_result = stream.read_to_end(&mut data_buf);
-            let data_sz = match read_result {
-                Ok(data_sz) => data_sz,
-                Err(e) => {
-                    eprintln!("failed to read from the tcp stream. error: {}", e);
-                    stream.shutdown(Shutdown::Both).unwrap_or(());
-                    continue;
-                },
-            };
-            // now, that the request data has been read in, construct an actual request instance
-            // from the byte slice
-            let req_result = Request::try_from(&data_buf[..data_sz]);
+            let address = self.address.clone();
+            thread::spawn(move || {
+                dbg!(format!("[+] new connection {} -> {}", &addr.to_string(), address));
+                let mut data_buf: Vec<u8> = Vec::new();
+                let read_result = stream.read_to_end(&mut data_buf);
+                let data_sz = match read_result {
+                    Ok(data_sz) => data_sz,
+                    Err(e) => {
+                        eprintln!("failed to read from the tcp stream. error: {}", e);
+                        stream.shutdown(Shutdown::Both).unwrap_or(());
+                        return;
+                    },
+                };
+                // now, that the request data has been read in, construct an actual request instance
+                // from the byte slice
+                let req_result = Request::try_from(&data_buf[..data_sz]);
+            });
         }
     }
 }
